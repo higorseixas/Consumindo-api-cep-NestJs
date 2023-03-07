5d67c1f26f768ac5700342015422ea52d6080957
higorseixas <higor.c.seixas@gmail.com>
2023-03-06 21:01:35 -0300

    feat: add function updateCepFlagByUf

--- src/cep/cep.service.ts
+++ src/cep/cep.service.ts
@@ -35,6 +35,7 @@ export class CepService {
               uf: data.uf,
               ibge: data.ibge,
               gia: data.gia,
+              flag: true,
               ddd: data.ddd,
               siafi: data.siafi,
             },
@@ -101,67 +102,19 @@ export class CepService {
       });
   }
 
-  // async getAllUsers() {
-  //   return this.prisma.user
-  //     .findMany({ select: { id: true, nome: true, email: true } })
-  //     .then((result) => result)
-  //     .catch((e) => {
-  //       console.log(e);
-  //     });
-  // }
-
-  // async getContactsByUserId(userId: number) {
-  //   return this.prisma.contact
-  //     .findMany({
-  //       where: { userId: userId },
-  //       select: { id: true, number: true, userId: true }
-  //     })
-  //     .then((result) => {
-  //       return result;
-  //     })
-  //     .catch((e) => {
-  //       console.log(e);
-  //     });
-  // }
-
-  // async createUser(nome: string, email: string) {
-  //   return this.prisma.user
-  //     .findUnique({
-  //       where: { email: email }
-  //     })
-  //     .then((existingUser) => {
-  //       if (existingUser) {
-  //         console.log(existingUser);
-  //         throw new Error('E-mail already registered.');
-  //       }
-  //       return this.prisma.user.create({
-  //         data: {
-  //           nome,
-  //           email
-  //         }
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       throw error;
-  //     });
-  // }
-
-  // async updateUser(id: number, email: string) {
-  //   return this.prisma.user
-  //     .findUnique({
-  //       where: { id: id }
-  //     })
-  //     .then((existingUser) => {
-  //       if (!existingUser) {
-  //         throw new Error('User does not exist.');
-  //       }
-  //       return this.prisma.user.update({
-  //         where: { id: id },
-  //         data: { email: email }
-  //       });
-  //     })
-  //     .catch((error) => {
-  //       throw error;
-  //     });
-  // }
+  async updateCepFlagByUf() {
+    const states = await this.prisma.state.findMany({});
+    for (const state of states) {
+      const uf = state.uf;
+      const ceps = await this.prisma.cep.findMany({
+        where: { uf },
+      });
+      for (const cep of ceps) {
+        await this.prisma.cep.update({
+          where: { id: cep.id },
+          data: { flag: true },
+        });
+      }
+    }
+  }
 }
